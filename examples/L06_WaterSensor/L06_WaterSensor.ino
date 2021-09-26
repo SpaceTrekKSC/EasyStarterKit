2fe14fdacd67690c5e6800b94989f8541c094cb3
Andrew Gafford <88450122+agafford-ST@users.noreply.github.com>
2021-09-26 09:47:27 -0500

    Update L06_WaterSensor.ino
    comments...

--- examples/L06_WaterSensor/L06_WaterSensor.ino
+++ examples/L06_WaterSensor/L06_WaterSensor.ino
@@ -1,44 +1,61 @@
-/************************************************* *************************************************
-* OPEN-SMART Easy Starter Kit Lesson 6:  Water Sensor
-* You can learn how to detect whether there is water. If it finds the water, the buzzer will sound.
+/**************************************************************************************************
+* Easy Starter Kit Lesson 6:  Water Sensor
+* In this lesson you will learn how to use the water sensor.  The water sensor will detect anything
+* that conducts electricity touching the silver lines on it.  You can test it by dipping it in water
+* or you can touch it with your fingers and is will change resistance.  The below example uses a
+* function to determine if the resistance is low enough to be considered exposed to water.
+* 
+* As a fun experiment, when you are done with this lesson see if you can modify it based on lesson 5
+* such that it outputs the analogRead() value to the serial terminal so that you can see in real time
+* what the voltage being read by the Arduino is.  Then experiment with different things contacting
+* the sensor to see how they effect it.
+* 
+* Use a 3 wire connector to connect the active buzzer to the blue port that has a D6 on its pin
+* labels.
+* 
+* Use a 3 wire connector to connect the water sensor to the blue port that has an A0 on its pin
+* labels.
+* 
+* The following functions are available: 
+*
+* buzzer.on();              //turn on the buzzer
+* buzzer.off();             //turn off the buzzer
+* 
+* isExposedToWater()        //use defined function to determine if the sensor is exposed to water
 *
 *
-************************************************** ************************************************** *****/
+***************************************************************************************************/
 
-#include <Wire.h>
-#include <SoftwareSerial.h>
-#include "EasyStarterKitBuzzer.h"
+#include <EasyStarterKitBuzzer.h>
 
 #define BUZZER_PIN 6         //SIG pin of Buzzer module connect to D6 of Arduino
 Buzzer buzzer(BUZZER_PIN);    
 
 #define WATER_SENSOR A0//SIG pin of Water Sensor module connect to A0 of IO Shield, that is pin A0 of Arduino
 
-void setup()
-{
-	
+//setup() runs once when the program is first started
+void setup(){
+  //this program does not do anything in the setup() function, but
+  //Arduino requires the setup() function to be in the program
 }
-void loop()
-{
-	if(isExposedToWater())
-	{
-		buzzer.on();
-		delay(100);
-	}
-	else buzzer.off();
 
+void loop(){                  //loop repeats over and over
+	if(isExposedToWater()){     // if isExposedToWater returns true
+		buzzer.on();              //turn the buzzer on
+		delay(100);               //and wait for 100 ms so the user has a chance to hear the buzzer
+	}
+	else buzzer.off();          //otherwise turn the buzzer off
 }
 
 
 /************************************************************************/
-/*Function: Determine whether the sensor is exposed to the water		*/
-/*Parameter:-void           											*/
-/*Return:	-boolean,if it is exposed to the water,it will return ture. */
-boolean isExposedToWater()
-{
-	if(analogRead(WATER_SENSOR) < 180)
-		return true;
-	else return false;
+/*Function: Determine whether the sensor is exposed to water            */
+/*Parameter: -void - you do not pass any argument parameters            */
+/*Return: -boolean, if it is exposed to the water,it will return true   */
+boolean isExposedToWater(){
+  if(analogRead(WATER_SENSOR) < 180)      //if the voltage on the analog pin assigned to WATER_SENSOR returns a value less than 180
+    return true;                          //return true
+  else return false;                      //otherwise return false
 }
 
 /*********************************************************************************************************

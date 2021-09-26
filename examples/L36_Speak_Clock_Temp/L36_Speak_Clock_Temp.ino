f82b0b3ec221856a50f5ada0026cfd99e93806b3
Andrew Gafford <88450122+agafford-ST@users.noreply.github.com>
2021-09-26 07:53:55 -0500

    Update L36_Speak_Clock_Temp.ino

--- examples/L36_Speak_Clock_Temp/L36_Speak_Clock_Temp.ino
+++ examples/L36_Speak_Clock_Temp/L36_Speak_Clock_Temp.ino
@@ -13,49 +13,49 @@
 *     // Folder names must be 01 02
 *     // Inside the folder each audio clip must be labeled 000 001 002 003 ...
 *     
-* timer1.initialize(unsigned long microseconds);      // set a timer for given microseconds
-* timer1.attachInterrupt (TimingISR);                 // sets a timing interrupt routine
+* timer1.initialize(unsigned long microseconds);        // set a timer for given microseconds
+* timer1.attachInterrupt (TimingISR);                   // sets a timing interrupt routine
 * 
-* temper.getTemperature();                            // returns the temperature value as a float
+* temper.getTemperature();                              // returns the temperature value as a float
 * 
-* lcd.init();                                         // I2C 1602 LCD screen initialization
-* lcd.backlight();                                    // turn on the back light of the screen
-* lcd.noBacklight();                                  // turn off the back light of the screen
-* lcd.setCursor(uint8_t col, uint8_t row);            // set the cursor position
-* lcd.print(Sring str);                               // display string on the screen
-* lcd.print(double n, int digits);                    // display a decimal number on the screen
+* lcd.init();                                           // I2C 1602 LCD screen initialization
+* lcd.backlight();                                      // turn on the back light of the screen
+* lcd.noBacklight();                                    // turn off the back light of the screen
+* lcd.setCursor(uint8_t col, uint8_t row);              // set the cursor position
+* lcd.print(Sring str);                                 // display string on the screen
+* lcd.print(double n, int digits);                      // display a decimal number on the screen
 * 
-* knob.getAngle();                                    // returns the degrees the knob is turned (0-280)
+* knob.getAngle();                                      // returns the degrees the knob is turned (0-280)
 * 
-* touch.get();                                        // returns the pin number of the touched area, 
-*                                                     // if nothing is touched it returns -1
+* touch.get();                                          // returns the pin number of the touched area, 
+*                                                       // if nothing is touched it returns -1
 *                                                     
-* mp3.getStatus();                                    // returns the status of MP3 player
-*                                                     // options include: PlayerSTOP, PLAY, PAUSE, FORWARD, REWIND
-* mp3.setVolume(int8_t vol);                          // vol is 0~0x1e, 30 adjustable level
-* mp3.playWithFileName(int8_t directory, int8_t file);// play a song according to the folder name and prefix of its file name
-*                                                     // folder name must be 01 02 03...09 10...99
-*                                                     // prefix of file name must be 001...009 010...255
-* clock.begin();                                      // this starts the clock
-* clock.getTime(void);                                // reads the clock and time, will be saved in the clock class variable
-*                                                     // The clock class variable contains the following:
-*                                                           // uint8_t second;	
-*                                                           // uint8_t minute;	
-*                                                           // uint8_t hour; 	
-*                                                           // uint8_t dayOfWeek;	
-*                                                           // uint8_t dayOfMonth;	
-*                                                           // uint8_t month;	
-*                                                           // uint16_t year;															
+* mp3.getStatus();                                      // returns the status of MP3 player
+*                                                       // options include: PlayerSTOP, PLAY, PAUSE, FORWARD, REWIND
+* mp3.setVolume(int8_t vol);                            // vol is 0~0x1e, 30 adjustable level
+* mp3.playWithFileName(int8_t directory, int8_t file);  // play a song according to the folder name and prefix of its file name
+*                                                       // folder name must be 01 02 03...09 10...99
+*                                                       // prefix of file name must be 001...009 010...255
+* clock.begin();                                        // this starts the clock
+* clock.getTime(void);                                  // reads the clock and time, will be saved in the clock class variable
+*                                                       // The clock class variable contains the following:
+*                                                             // uint8_t second;	
+*                                                             // uint8_t minute;	
+*                                                             // uint8_t hour; 	
+*                                                             // uint8_t dayOfWeek;	
+*                                                             // uint8_t dayOfMonth;	
+*                                                             // uint8_t month;	
+*                                                             // uint16_t year;															
 ***********************************************************************************************************/
 #include <TimerOne.h>                     // include the library for the timer
 
-#include "EasyStarterKitMP3.h"            // include the MP3 library
-#include "EasyStarterKitTouchSensor.h"    // include the touch sensor library
-#include "EasyStarterKitDS1307.h"         // include the clock library
-#include "EasyStarterKitBuzzer.h"         // include the buzzer library
-#include "EasyStarterKitTemperature.h"    // include the temperature sensor library
-#include "EasyStarterKitKnob.h"           // include the rotary knob library
-#include "EasyStarterKit_LCD1602.h"       // include the LCD screen library
+#include <EasyStarterKitMP3.h>            // include the MP3 library
+#include <EasyStarterKitTouchSensor.h>    // include the touch sensor library
+#include <EasyStarterKitDS1307.h>         // include the clock library
+#include <EasyStarterKitBuzzer.h>         // include the buzzer library
+#include <EasyStarterKitTemperature.h>    // include the temperature sensor library
+#include <EasyStarterKitKnob.h>           // include the rotary knob library
+#include <EasyStarterKit_LCD1602.h>       // include the LCD screen library
 
 LiquidCrystal_I2C lcd(0x20);      // set the LCD address to 0x20 for the 16 chars and 2 line display (default setting)
 							                    // if 0x20 does not work, please modify it to be 0x38
@@ -109,39 +109,39 @@ void setup() {      // start setup(), setup() runs once when the program starts
   updateVolume();                       // set volume based on knob rotation
 }
 
-void loop() {
-  int button;
-  uint8_t flag_speak_time = 0;      // 0 = not speak, 1 = to speak
-  uint8_t flag_speak_temp = 0;      // 0 = not speak, 1 = to speak
-  button = touch.get();             // store the value of touched area in "button"
-  if(button != -1){                 // if something was touched
-    delay(10);                      // delay for 10ms
-    buzzer.on();                    // sound the buzzer
-    if(button == TOUCH_OUT1){       // if touched area corresponds to TCH1 area
-      flag_speak_temp = 1;          // set the flag to read the temperature
+void loop() {                           // loop repeats over and over
+  int button;                           // declare a variable named button of type int 
+  uint8_t flag_speak_time = 0;          // 0 = not speak, 1 = to speak
+  uint8_t flag_speak_temp = 0;          // 0 = not speak, 1 = to speak
+  button = touch.get();                 // store the value of touched area in "button"
+  if(button != -1){                     // if something was touched
+    delay(10);                          // delay for 10ms
+    buzzer.on();                        // sound the buzzer
+    if(button == TOUCH_OUT1){           // if touched area corresponds to TCH1 area
+      flag_speak_temp = 1;              // set the flag to read the temperature
     }
-    else if(button == TOUCH_OUT2){  // if touched area corresponds to TCH2 area
-      flag_speak_time = 1;          // set the flag to read the time
+    else if(button == TOUCH_OUT2){      // if touched area corresponds to TCH2 area
+      flag_speak_time = 1;              // set the flag to read the time
     }
-  	delay(50);                      // delay for 50ms
-  	buzzer.off();                   // turn the buzzer off
-  	while(button == touch.get());   // Wait for the button to be released
+  	delay(50);                          // delay for 50ms
+  	buzzer.off();                       // turn the buzzer off
+  	while(button == touch.get());       // Wait for the button to be released
   }
   float celsius;
-  if(Update == ON){                 // update the display if interrupt timer says to
-    TimeUpdate();                   // get the updated time
-	  celsius = temper.getTemperature();// get temperature
-    displayTemperature(celsius);    // display updated temperature
-    Update == OFF;                  // set Update flag to off
+  if(Update == ON){                     // update the display if interrupt timer says to
+    TimeUpdate();                       // get the updated time
+	  celsius = temper.getTemperature();  // get temperature
+    displayTemperature(celsius);        // display updated temperature
+    Update == OFF;                      // set Update flag to off
   }
-  updateVolume();                   // set volume based on current angle of knob
-  if(flag_speak_time){              // if speak time flag was changed
-    SpeakTime(Time);                // speak the current time
-	  flag_speak_time = 0;            // set the speak time flag to false
+  updateVolume();                       // set volume based on current angle of knob
+  if(flag_speak_time){                  // if speak time flag was changed
+    SpeakTime(Time);                    // speak the current time
+	  flag_speak_time = 0;                // set the speak time flag to false
   }
-  if(flag_speak_temp){              // if speak temp flag was changed
-    SpeakTemp(celsius);             // speak the current temperature
-	  flag_speak_temp = 0;            // set speak temp flag to false
+  if(flag_speak_temp){                  // if speak temp flag was changed
+    SpeakTemp(celsius);                 // speak the current temperature
+	  flag_speak_temp = 0;                // set speak temp flag to false
   } 
 }
 
@@ -149,7 +149,7 @@ void loop() {
 
 // This function updates the volume based on current angle of the knob
 void updateVolume(){
-  int angle;
+  int angle;                                      // declare a variable named angle of type int
   angle = knob.getAngle();                        // returns the angle of the knob
   newVolume = map(angle, 0, FULL_ANGLE, 0, 30);   // chooses volume based on that measurement
   if(newVolume != oldVolume){                     // if volumes aren't the same
@@ -182,32 +182,32 @@ void SpeakTime(int8_t time[]){
   	  if(Time[3] != 0)addr[next++] = Time[3] + NUM_OFFSET;  // then add the second digit normally
   	}
   }
-  SpeakGroup(addr, next);                                 // send the array we constructed to the function to read it
+  SpeakGroup(addr, next);                                   // send the array we constructed to the function to read it
 }
 
 // This function reads the current temperature
 void SpeakTemp(float temp){
-  if(temp >= 1000)return;           // we only have audio files to support up to 999
-  else if(temp <= -1000)return;     // however, if the reading was this temperature this is surely an issue
+  if(temp >= 1000)return;                 // we only have audio files to support up to 999
+  else if(temp <= -1000)return;           // however, if the reading was this temperature this is surely an issue
   
-  uint8_t addr[10] = {0};           // we use this array to construct the sentences
+  uint8_t addr[10] = {0};                 // we use this array to construct the sentences
   uint8_t next = 0;
-  addr[next++] = 31;                // 031 plays a bell before speaking
-  addr[next++] = 35;                // 035 says "the temperature is"
-  if(temp < 0){                     // if the temperature is negative
-    temp = abs(temp);               // take the absolute value
-	  addr[next++] = 40;              // 040 says "minus"
+  addr[next++] = 31;                      // 031 plays a bell before speaking
+  addr[next++] = 35;                      // 035 says "the temperature is"
+  if(temp < 0){                           // if the temperature is negative
+    temp = abs(temp);                     // take the absolute value
+	  addr[next++] = 40;                    // 040 says "minus"
   }
-  int t = temp;                     // cast the temperature as an int (truncate the decimals)
+  int t = temp;                           // cast the temperature as an int (truncate the decimals)
   uint8_t flag_hundred;
-  if(t >= 100){                     // if temperature is greater than 100
-    flag_hundred = 1;               // set the flag to true
-  	addr[next++] = t / 100 + NUM_OFFSET;// add audio for digit before hundred (1, 2, 3...)
-	  addr[next++] = 30;              // add clip 030 to array it says "hundred"
-	  t %= 100;                       // get the remainder of the temperature
+  if(t >= 100){                           // if temperature is greater than 100
+    flag_hundred = 1;                     // set the flag to true
+  	addr[next++] = t / 100 + NUM_OFFSET;  // add audio for digit before hundred (1, 2, 3...)
+	  addr[next++] = 30;                    // add clip 030 to array it says "hundred"
+	  t %= 100;                             // get the remainder of the temperature
   }
-  else flag_hundred = 0;            // if temp is lower than 100 set flag to false
-  if(t != 0){                       // if temp is not 0
+  else flag_hundred = 0;                  // if temp is lower than 100 set flag to false
+  if(t != 0){                             // if temp is not 0
    	if(flag_hundred)addr[next++] = 38;    // if temp is above hundred say "and" with clip 038
   	if(t < 20){                           // if temp remaining is less than 20
   	  addr[next++] = t + NUM_OFFSET;      // add to array as normal
@@ -219,13 +219,13 @@ void SpeakTemp(float temp){
   	}
   }
  
-  addr[next++] = 1;                     // add clip 001 which says "point"
+  addr[next++] = 1;                       // add clip 001 which says "point"
  
-  uint8_t subbit;
-  subbit = ((int)(temp*10))%10;         // multiply float by 10, then modulus by ten to get the last digit
-  addr[next++] = subbit + NUM_OFFSET;   // add this to array for the degree in decimal
-  addr[next++] = 36;                    // add clip 036 to say "degrees Celsius"
-  SpeakGroup(addr, next);               // send this array and the amount of items (stored in next) to the function to speak it
+  uint8_t subbit;                         // declare a variable named subbit of type unsigned 8 bit int
+  subbit = ((int)(temp*10))%10;           // multiply float by 10, then modulus by ten to get the last digit
+  addr[next++] = subbit + NUM_OFFSET;     // add this to array for the degree in decimal
+  addr[next++] = 36;                      // add clip 036 to say "degrees Celsius"
+  SpeakGroup(addr, next);                 // send this array and the amount of items (stored in next) to the function to speak it
 }
 
 // This function plays out constructed sentences audio files.
@@ -237,18 +237,18 @@ void SpeakGroup(uint8_t addr[], uint8_t size){
   while(mp3.getStatus()!=STATUS_STOP)delay(50);    // wait 50ms after it finishes the sentence
 }
 
-void displayTemperature(float temp){
-  if((temp<-40)||(temp>125))return;   // if temperature is unreasonable exit the function
-  lcd.setCursor(0, 1);                // set cursor to the first column, second row
-  lcd.print("Temp: ");                // print "Temp: " to the screen
-  if(temp < 0){                       // if the temperature is negative
-    temp = abs(temp);                 // take absolute value
-    lcd.print("-");                   // add the "-" to the screen 
+void displayTemperature(float temp){                // this function displays temp on the lcd display
+  if((temp<-40)||(temp>125))return;                 // if temperature is unreasonable exit the function
+  lcd.setCursor(0, 1);                              // set cursor to the first column, second row
+  lcd.print("Temp: ");                              // print "Temp: " to the screen
+  if(temp < 0){                                     // if the temperature is negative
+    temp = abs(temp);                               // take absolute value
+    lcd.print("-");                                 // add the "-" to the screen 
   }
-  if((temp < 100)&&(temp>=10))lcd.print(" ");   // if the temp is two digits, print a space for hundredths place
-  else if(temp < 10)lcd.print("  ");            // if the temp is one digit, print two spaces
-  lcd.print(temp,1);                            // print the temperature
-  lcd.print(" 'C");                             // add the Celsius symbol to the screen
+  if((temp < 100)&&(temp>=10))lcd.print(" ");       // if the temp is two digits, print a space for hundredths place
+  else if(temp < 10)lcd.print("  ");                // if the temp is one digit, print two spaces
+  lcd.print(temp,1);                                // print the temperature
+  lcd.print(" 'C");                                 // add the Celsius symbol to the screen
 } 
 
 /********** Timer1 Interrupt Routine **********/
@@ -257,7 +257,7 @@ void TimingISR(){
   ClockPoint = !ClockPoint;   // Set checkpoint to opposite of whatever it is now
 }
 
-void TimeUpdate(void){
+void TimeUpdate(void){                    // this function updates the time on the lcd
   clock.getTime();                        // get the current time
   Time[0] = clock.hour / 10;              // store the hour's first digit
   Time[1] = clock.hour % 10;              // store the hour's second digit

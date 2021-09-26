098d50a497068e4c3c4827bac417a1ea0f3a51aa
Andrew Gafford <88450122+agafford-ST@users.noreply.github.com>
2021-09-26 10:43:46 -0500

    Update L08_Buzzer_PlayTone.ino
    comments...

--- examples/L08_Buzzer_PlayTone/L08_Buzzer_PlayTone.ino
+++ examples/L08_Buzzer_PlayTone/L08_Buzzer_PlayTone.ino
@@ -1,25 +1,31 @@
 /************************************************* *************************************************
-* OPEN-SMART Easy Starter Kit Lesson 8:  Passive buzzer to play tone of Chinese Double Tiger
-* You can learn how to use a passive buzzer to play simple songs according to the notation.
+* Easy Starter Kit Lesson 8:  Passive buzzer to play tone of Chinese Double Tiger song
+* In this lesson you will learn how to use the passive buzzer.  Unlike the active buzzer used in the
+* previous examples, the passive buzzer is able to produce different tones depending on its input.
+* The active buzzer generates its own oscillating signal when it is activated.  The passive buzzer
+* needs to have an oscillating signal sent to its input, it is more like a speaker than the active
+* buzzer.  The passive buzzer is a blue board with a black circular component.  It is the one that 
+* did not come with the sticker on it.
+* 
+* Use a 3 wire connector to connect the passive buzzer to the blue port with a D6 on its pin label
 *
 * The following functions are available:
 *
-* playTone(long note, long millisecond); //The first parameter note is the frequency of note, unit is HZ; 
-*                                       //millisecond is the duration of this tone, unit is ms.
+* buz.playTone(long note, long millisecond);    //The first parameter note is the frequency of note, unit is HZ; 
+*                                               //millisecond is the duration of this tone, unit in ms.
+*
+* buz.off();                                    //turns off the buzzer*                                               
 *
 ************************************************** *******************************************************/
-
-#include <Wire.h>
-#include <SoftwareSerial.h>
 	 
-#include "EasyStarterKitPassiveBuzzer.h"
+#include <EasyStarterKitPassiveBuzzer.h>      //include the passive buzzer library
 
-#define PassiveBuzzerPin 6//the SIG pin of the module is connected with D6 of Arduino / OPEN-SMART UNO
-                     //There is no white waterproof sticker on the passive buzzer
-PassiveBuzzer buz(PassiveBuzzerPin);
+#define PassiveBuzzerPin 6                    //the SIG pin of the module is connected with D6 of Arduino / OPEN-SMART UNO
+                                              //There is no white waterproof sticker on the passive buzzer
+PassiveBuzzer buz(PassiveBuzzerPin);          //setup the passive buzzer object
 
 //------------------------------------
-//This is a macro definition of bass, midrange, and treble frequencies
+//This is a macro definition of bass, mid-range, and treble frequencies
 #define NOTE_L1  262
 #define NOTE_L2  294
 #define NOTE_L3  330
@@ -42,8 +48,8 @@ PassiveBuzzer buz(PassiveBuzzerPin);
 #define NOTE_H6  1760
 #define NOTE_H7  1976
 //-------------------------------------------
-//musical notation of Two Tigers 
-const static int doubleTiger[] = { 
+//musical notation of the song Two Tigers 
+const static int doubleTiger[] = {          //create a constant static array containing the notes to play
 NOTE_M1,NOTE_M2,NOTE_M3,NOTE_M1, 
 NOTE_M1,NOTE_M2,NOTE_M3,NOTE_M1, 
 NOTE_M3,NOTE_M4,NOTE_M5, 
@@ -58,8 +64,10 @@ NOTE_M2,NOTE_L5,
 NOTE_M1,0
 };
 //-------------------------------------------
+
+
 ////note duration of Two Tigers, 2 is half a beat, 1 is a beat.
-const static int tempo[] = { 
+const static int tempo[] = {              //create a constant static array of the beats needed for each note
 2,2,2,2, 
 2,2,2,2, 
 2,2,1, 
@@ -74,31 +82,32 @@ const static int tempo[] = {
 1,1
 };
 
-void setup(void)
-{
-  //sing the tunes
-  sing();
-}
-void loop()
-{
-
+void setup(void){   //setup() runs once.  By calling sing() here we make it so it will only play once
+  sing();           //call the user defined function sing() to play the song
 }
 
-void sing() {
-  // iterate over the notes of the melody:
-    int size = sizeof(doubleTiger) / sizeof(int);
-    for (int thisNote = 0; thisNote < size; thisNote++) {
-      int noteDuration = 500 / tempo[thisNote];//a beat for 0.5 second
+//loop repeats over and over.  If you put the sing() command in loop the song will play forever
+void loop(){
+  //this program does not do anything in the loop() function, but
+  //Arduino requires the loop() function to be in the program
+}
 
-      buz.playTone(doubleTiger[thisNote], noteDuration);
+void sing(){      // iterate over the notes of the melody:
+  int size = sizeof(doubleTiger) / sizeof(int);           //declare a variable named size of type int and initialize it to 
+                                                          // the size of the doubleTiger array divided by the size of an int
+                                                          //so that we have the number of values in the array
+                                                          
+  for (int thisNote = 0; thisNote < size; thisNote++){    //create a for loop to iterate through each of the elements in the array
+    int noteDuration = 500 / tempo[thisNote];             //create a variable for the beat of the tone to be played using a beat of 0.5 seconds
 
-      // to distinguish the notes, set a minimum time between them.
-      // the note's duration + 30% seems to work well:
-      int pauseBetweenNotes = noteDuration * 0.30;
-      delay(pauseBetweenNotes);
-    }
+    buz.playTone(doubleTiger[thisNote], noteDuration);    //play the current tone for the current beat time
+
+    // to distinguish the notes, set a minimum time between them.
+    // the note's duration + 30% seems to work well:
+    int pauseBetweenNotes = noteDuration * 0.30;
+    delay(pauseBetweenNotes);
+  }
 }
 /*********************************************************************************************************
 The end of file
 *********************************************************************************************************/
-

ba571069f0e9b2914a0079b9e181b6cc9ac49259
Andrew Gafford <88450122+agafford-ST@users.noreply.github.com>
2021-10-06 16:21:34 -0500

    Update L20_DisplayClock1602.ino
    changed to use RTClib
    updated comments...

--- examples/L20_DisplayClock1602/L20_DisplayClock1602.ino
+++ examples/L20_DisplayClock1602/L20_DisplayClock1602.ino
@@ -1,109 +1,121 @@
-/************************************************* ************************************************** ******
-* OPEN-SMART Easy Starter Kit Lesson 20: I2C 1602 LCD Display Clock
-* You can learn to use I2C 1602 LCD to display time.
+/*******************************************************************************************************
+* Easy Starter Kit Lesson 20: I2C 1602 LCD Display Clock
+* 
+* In this lesson you will learn how to display the time from a Real Time Clock (RTC) on the 1602 LCD.
+* It is similar to lesson 19 except now we are only adjusting the time if the RTC is not running and
+* we are using the LCD display instead of the 4 digit display.
+* 
+* Use a 4 wire connector to connect the RTC to one of the yellow I2C ports
+* Use a 4 wire connector to connect the LCD to the other yellow I2C port
 *
 * The following functions are available:
+*
+* Timer1.initialize(unsigned long microseconds);  //set the timed length, the unit is microseconds
+* Timer1.attachInterrupt(TimingISR);              //set interrupt routine function name, is the timing interrupt entry
+*
+* lcd.init();                                     //I2C 1602 LCD screen initialization
+* lcd.backlight();                                //turn on the backlight
+* lcd.noBacklight();                              //turn off the backlight
+* lcd.setCursor(uint8_t col, uint8_t row);        //set the cursor position,col is column
+* lcd.print(String str);                          //display string
 
-* Timer1.initialize(unsigned long microseconds); // set the timed length, the unit is microseconds
-* Timer1.attachInterrupt(TimingISR); // set interrupt routine function name, is the timing interrupt entry
+* clock.begin();                  //The clock is on and the function must be called first
+* clock.isrunning()               //returns true if the RTC is running or false if it is not
+* clock.adjust(DateTime());       //adjusts the date and time on the RTC chip
+* now = clock.now();              //sets the now object to the current date and time from the RTC
+* 
+* now.hour()                      //access the hour from the now object in 24 hour format
+* now.twelveHour()                //access the hour from the now object in 12 hour format
+* now.minute()                    //access the minute from the now object
+* now.second()                    //access the second from the now object
+* now.year()                      //access the year from the now object
+* now.month()                     //access the month from the now object
+* now.day()                       //access the day from the now object
+* 
+*******************************************************************************************************/
 
-* lcd.init();     // I2C 1602 LCD screen initialization
-* lcd.backlight();//turn on the backlight
-* lcd.noBacklight();//turn off the backlight
-* lcd.setCursor(uint8_t col, uint8_t row);// set the cursor position,col is column
-* lcd.print(Sring str);//display string
+#include <TimerOne.h>                   //include the TimerOne library
+#include <Wire.h>                       //include the I2C library
+#include <RTClib.h>                     //include the Real Time Clock library
+#include <EasyStarterKit_LCD1602.h>     //include the LCD library
 
-* clock.begin(); // The clock is on and the function must be called first
-* clock.startClock(void); // clock start, if you stop the clock need to call this function
-* clock.stopClock(void); // clock stop timing
-* clock.getTime(void); / / read the clock and time, will be saved in the clock class variable
-* clock.fillByHMS(uint8_t _hour, uint8_t _minute, uint8_t _second); // write minutes
-* clock.fillByYMD(uint16_t _year, uint8_t _month, uint8_t _day); // write clock
-* clock.fillDayOfWeek(uint8_t _dow); // write week
-* clock.setTime(void); // write the clock and time that have been written to the clock chip
-************************************************** **************************************************/
+LiquidCrystal_I2C lcd(0x20);            //set the LCD address to 0x20 for the 16 chars and 2 line display (default setting)
+                                        //if 0x20 does not work, please modify it to be 0x38
 
-#include <TimerOne.h>
-#include <Wire.h>
-#include <SoftwareSerial.h>
+RTC_DS1307 clock;                       //setup a RTC-DS1307 object named clock
+DateTime now;                           //setup am object variable called now of user defined type DateTime which is defined in the RTClib library
 
-#include "EasyStarterKitDS1307.h"
-#include "EasyStarterKit_LCD1602.h"
+#define ON 1                            //define ON to be 1 (true)
+#define OFF 0                           //define OFF to be 0 (false)
 
-LiquidCrystal_I2C lcd(0x20);  // set the LCD address to 0x20 for the 16 chars and 2 line display (default setting)
-							// if 0x20 does not work, please modify it to be 0x38
+volatile uint8_t Update = ON;           //declare and initialize the variable Update.  Update is used in an interrupt service routine
+                                        //so it is good practice to specify that the variable is volatile.
 
-DS1307 clock;//define a object of DS1307 object
+void setup(){                           //setup runs once when the program is first started
+  clock.begin();                        //initialize the RTC object called clock
 
-#define ON 1
-#define OFF 0
+  /***********************************************************************************/
+  /* In lesson 19 we called clock.adjust() every time we ran the program.            */
+  /* Now we are going to put it inside an if statement and only call it if the       */
+  /* RTC is not running.  Remember, this is the time that the program was compiled   */
+  /* so if you take the battery out it will reset the clock to the time the          */
+  /* program was compiled                                                            */
+  /***********************************************************************************/
+  if(!clock.isrunning()){                               //if the clock is not running (i.e. it was reset by removing the battery)
+    clock.adjust(DateTime(F(__DATE__), F(__TIME__)));   //update the RTC to the date and time of when the program was compiled
 
-uint8_t Update = ON;
-uint8_t halfsecond = 0;
-
-void setup() {
-  clock.begin();
-  
-//  clock.fillByYMD(2019,8,3);//2017.08.09
-//  clock.fillByHMS(15,13,10);//17:01:10
-//  clock.fillDayOfWeek(SAT);//Saturday,The first three letters
-//  clock.setTime();//write time to the RTC chip
-
-  lcd.init();	 // initialize the lcd 
-  lcd.backlight();//turn on the backlight
-  
-  Timer1.initialize(500000);//timing for 500ms
-  Timer1.attachInterrupt(TimingISR);//declare the interrupt serve routine:TimingISR 
-  
-}
-
-void loop() {
-
-  if(Update == ON)
-  {
-    DataUpdate();
+    //clock.adjust(DateTime(2021, 10, 6, 13, 14, 38));  //Or use this line to set the RTC with an explicit date & time.
+                                                        //For example, to set the RTC to October 6, 2021 at 1:14:38pm
   }
   
+  //or you can use them outside of the if statement to always reset the clock to the compile time
+  //clock.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  //clock.adjust(DateTime(2021, 10, 6, 13, 14, 38));
+
+  lcd.init();                                 //initialize the lcd 
+  lcd.backlight();                            //turn on the backlight
+  
+  Timer1.initialize(1000000);                 //timing for 1 second
+  Timer1.attachInterrupt(TimingISR);          //declare the interrupt serve routine:TimingISR 
+  
 }
 
-void TimingISR()
-{
-  halfsecond++;
-  if(halfsecond == 2)
-  {
-    Update = ON;
-	halfsecond = 0;
+void loop(){              //loop repeats over and over
+  if(Update == ON){       //if Update is set to ON
+    DataUpdate();         //then update the display
   }
 }
 
+void TimingISR(){         //the timer interrupt function
+  Update = ON;            //whenever the timer goes off set Update to on
+}
 
-void DataUpdate()
-{
-  clock.getTime();
-  lcd.setCursor(0, 0);//the first place of the first line
-  lcd.print("Time: ");
-  if(clock.hour < 10)lcd.print(" ");
-  lcd.print(clock.hour, DEC);
-  lcd.print(":");
-  if(clock.minute < 10)lcd.print("0");
-  lcd.print(clock.minute, DEC);
-  lcd.print(":");
-  if(clock.second < 10)lcd.print("0");
-  lcd.print(clock.second, DEC);
+void DataUpdate(){                          //a user defined function to update the LCD with the current time and date from the RTC
+  now = clock.now();                        //get the time and date from the RTC and store it in the variable now
+  lcd.setCursor(0, 0);                      //set the cursor to the first place of the first line
+  lcd.print("Time: ");                      //print to "Time: "
+  //if(now.hour() < 10)lcd.print(" ");        //print the leading space if needed
+  //lcd.print(now.hour(), DEC);               //print the hour in 24 hour format
+  if(now.twelveHour() < 10)lcd.print(" ");  //print the leading space if needed
+  lcd.print(now.twelveHour(), DEC);         //print the hour in 12 hour format
+  lcd.print(":");                           //print the colon between hour and minute
+  if(now.minute() < 10)lcd.print("0");      //print the leading zero if needed
+  lcd.print(now.minute(), DEC);             //print the minute
+  lcd.print(":");                           //print the colon between minute and second
+  if(now.second() < 10)lcd.print("0");      //print the leading zero if needed
+  lcd.print(now.second(), DEC);             //print the second
   
-  lcd.setCursor(0, 1);//the first place of the second line
-  lcd.print("Date: ");
-  lcd.print(clock.year+2000, DEC);
-  lcd.print(".");
-  if(clock.month < 10)lcd.print("0");
-  lcd.print(clock.month, DEC);
-  lcd.print(".");
-  if(clock.dayOfMonth < 10)lcd.print("0");
-  lcd.print(clock.dayOfMonth, DEC);
+  lcd.setCursor(0, 1);                      //the first place of the second line
+  lcd.print("Date: ");                      //print "Date: "
+  lcd.print(now.year(), DEC);               //print the year
+  lcd.print("/");                           //print the / between year and month
+  if(now.month() < 10)lcd.print("0");       //print the leading zero if needed
+  lcd.print(now.month(), DEC);              //print the month
+  lcd.print("/");                           //print the / between month and day
+  if(now.day() < 10)lcd.print("0");         //print the leading zero if needed
+  lcd.print(now.day(), DEC);                //print the day
   
-  
-	
-  Update = OFF;
+  Update = OFF;                             //turn Update off
 }
 /*********************************************************************************************************
 The end of file
